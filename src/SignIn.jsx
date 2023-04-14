92ab2decaa95ebead6acb307eee434c5652805c4
UHyun-K <konglim2@naver.com>
2023-04-15 07:48:23 +0900

    [FEAT]: Signin api

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 
 //로그인 기능
 export default function SignIn() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [message, setMessage] = useState({
+        loginMessage: "",
+        login: false,
+    });
+    const navigate = useNavigate();
 
     const onEmailChnage = (event) => {
         const {
@@ -17,6 +23,41 @@ export default function SignIn() {
         } = event;
         return setPassword(value);
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(`https://www.pre-onboarding-selection-task.shop/auth/signin`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                //오류시
+                console.log("data:", data);
+                if (data.statusCode >= 400) {
+                    setMessage({
+                        login: false,
+                        loginMessage: data.message,
+                    });
+                    return;
+                }
+                //정상
+                localStorage.setItem("loginToken", data.access_token);
+                navigate("/todo");
+                setMessage({
+                    loginMessage: "로그인 성공",
+                    login: true,
+                });
+            })
+            .catch((error) => console.log(error));
+    };
+
     return (
         <>
             <h1>로그인</h1>
