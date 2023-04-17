8d731872bc42f22a4c77475159dcae3b06a85f90
UHyun-K <konglim2@naver.com>
2023-04-17 12:56:03 +0900

    [FEAT]: toDo생성하기

--- src/pages/ToDos.jsx
+++ src/pages/ToDos.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function ToDos() {
+    const [toDos, setToDos] = useState([]);
+    const [toDo, setToDo] = useState("");
+    const inputRef = useRef();
+    console.log("toDos:", toDos);
+    const handleValid = (e) => {
+        e.preventDefault();
+        setToDos((oldToDos) => [{ text: toDo, id: Date.now() }, ...oldToDos]);
+        setToDo("");
+    };
+    const onChange = (e) => {
+        const {
+            currentTarget: { value },
+        } = e;
+        setToDo(value);
+    };
     return (
         <div>
             <li>
                 <label>
-                    <input type="checkbox" data-testid="new-todo-input" />
+                    <input type="checkbox" />
                     <span>TODO 1</span>
                 </label>
             </li>
@@ -15,8 +30,21 @@ export default function ToDos() {
                     <span>TODO 2</span>
                 </label>
             </li>
-
-            <button data-testid="new-todo-add-button">추가</button>
+            <div>
+                <form onSubmit={handleValid}>
+                    <input
+                        type="text"
+                        data-testid="new-todo-input"
+                        placeholder="내용을 입력해주세요"
+                        value={toDo}
+                        ref={inputRef}
+                        onChange={onChange}
+                    />
+                    <button type="submit" data-testid="new-todo-add-button">
+                        추가
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
